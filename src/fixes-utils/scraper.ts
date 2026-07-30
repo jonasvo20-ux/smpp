dd853924bf3fe229d8b1b7de16f991db886a6a4c
Broodje565 <200642566+Broodje565@users.noreply.github.com>
2026-07-30 20:43:50 +0200

    scraper.ts omgezet naar typescript

--- src/fixes-utils/scraper.ts
+++ src/fixes-utils/scraper.ts
@@ -1,22 +1,21 @@
-// @ts-nocheck
-function scrape_from_html(query, func) {
-  data = [];
+function scrape_from_html(query: string, func: (el: Element, data: any[]) => void): any[] {
+  const data: any[] = [];
   let scrape_els = document.querySelectorAll(query);
-  if (scrape_els.length == 0) {
+  if (scrape_els.length === 0) {
     return data;
   }
   for (let i = 0; i < scrape_els.length; i++) {
-    func(scrape_els[i], data);
+    func(scrape_els[i]!, data);
   }
 
   return data;
 }
 
-function is_valid_data(data) {
-  return !(data == undefined || data.length == 0 || data.length == undefined);
+function is_valid_data(data: any): boolean {
+  return !(data === undefined || data.length === 0 || data.length === undefined);
 }
 
-function scrape_data_if_needed(data, query, func, done_func) {
+function scrape_data_if_needed(data: any, query: string, func: (el: Element, data: any[]) => void, done_func: (data: any[]) => void, name: string): boolean {
   if (is_valid_data(data)) {
     done_func(data);
     window.localStorage.setItem(name, JSON.stringify(data));
@@ -33,17 +32,18 @@ function scrape_data_if_needed(data, query, func, done_func) {
   return false;
 }
 
-function scrape(name, query, func, done_func, interval_time = 0) {
-  let data = JSON.parse(window.localStorage.getItem(name));
-  if (scrape_data_if_needed(data, query, func, done_func)) {
+function scrape(name: string, query: string, func: (el: Element, data: any[]) => void, done_func: (data: any[]) => void, interval_time: number = 0): void {
+  const raw = window.localStorage.getItem(name);
+  let data: any = raw !== null ? JSON.parse(raw) : null;
+  if (scrape_data_if_needed(data, query, func, done_func, name)) {
     return;
   }
-  if (interval_time == 0) {
+  if (interval_time === 0) {
     return;
   }
 
   let interval = setInterval(() => {
-    if (scrape_data_if_needed(data, query, func, done_func)) {
+    if (scrape_data_if_needed(data, query, func, done_func, name)) {
       clearInterval(interval);
     }
   }, interval_time);
